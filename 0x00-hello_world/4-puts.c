df3c45591d37111df0ca765b63139412188d4f46
tech-deacon <wajo9860@gmail.com>
2023-02-19 13:34:12 -0800

    tryys

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-int main()
+
+/**
+* main-prints a line of code
+* my first c code
+*/
+int main(void)
 {
-	puts("\"Programming is like building a mul		tilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
-	return 0;
+	return (0);
 }
