c85005c82b126ec703c69a7698c06246e407e3a0
tech-deacon <wajo9860@gmail.com>
2023-02-21 14:11:29 -0800

    won

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+* main-print size
+* return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld bytes(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
+	printf("size of a float: %ld byte(s)\n", sizeof(f);
+	return (0);
+}
