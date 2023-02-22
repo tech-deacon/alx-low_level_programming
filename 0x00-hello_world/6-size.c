114693e5a4ffe3980a313df0765135937e87398f
tech-deacon <wajo9860@gmail.com>
2023-02-21 21:27:46 -0800

    gill

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,9 @@
 
 /**
  * main-print size
+ *
+ * Description: using main function
+ * prints size
  * return: 0
  */
 int main(void)
@@ -12,7 +15,7 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("size of a char: %ld bytes(s)\n", sizeof(c));
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
