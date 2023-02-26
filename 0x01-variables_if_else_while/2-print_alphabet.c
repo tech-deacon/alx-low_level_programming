d4f7def72e2eeec74eccb4d92591bc152977abd3
tech-deacon <wajo9860@gmail.com>
2023-02-26 11:14:00 -0800

    fhg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print somethong inputed
+ *
+ * Description: using the main function
+ * the program prints accurqate
+ * Return: 0
+ */
+int main(void);
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch)
+}
+putchar('\n');
+return (0);
+}
