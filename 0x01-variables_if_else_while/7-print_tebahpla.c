0f75d557cbd5b35ab484c49bea6dec448da0305d
tech-deacon <wajo9860@gmail.com>
2023-02-26 12:20:33 -0800

    try

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print somethong inputed
+ *
+ * Description: using the main function
+ * the program prints accurqate
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
