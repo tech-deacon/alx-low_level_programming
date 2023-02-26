96e06bb20626b2a88a355c039134fc351a83b18f
tech-deacon <wajo9860@gmail.com>
2023-02-26 12:26:27 -0800

    hutr

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print somethong inputed
+ *
+ * Description: using the main function
+ * the program prints accurqate
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c)
+}
+putchar('\n');
+return (0);
+}
