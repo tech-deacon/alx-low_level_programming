f49349b2deacb143cf864333a5617c3dd10d8745
tech-deacon <wajo9860@gmail.com>
2023-02-26 12:07:42 -0800

    up

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print somethong inputed
+ *
+ * Description: using the main function
+ * the program prints accurqate
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf('\n');
+return (0);
+}
