4845b1eb7143683715796065aae8ec5b7ffa0343
tech-deacon <wajo9860@gmail.com>
2023-02-26 12:54:39 -0800

    gkl

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print somethong inputed
+ *
+ * Description: using the main function
+ * the program prints accurqate
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
